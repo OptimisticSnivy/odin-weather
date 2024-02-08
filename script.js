5fa482e15c5129c60f06e74eac510c4264c833ef
Anand Mahamuni <m.anand7203@gmail.com>
2024-02-08 20:37:38 +0530

    feat: temp_c && temp_f

--- script.js
+++ script.js
@@ -7,8 +7,19 @@ function getCity() {
 			inputValue = searchItem.value;
 			searchItem.value = "";
 			console.log(inputValue);
+			getTemp(inputValue);
 		}
 	});
+}
+
+async function getTemp(input) {
+	let response = await fetch(
+		"https://api.weatherapi.com/v1/current.json?key=91097dd388e84789b02131324240702&q="+input,
+		{ mode: "cors" },
+	);
+	let json = await response.json();
+	console.log(json.current.temp_c);
+	console.log(json.current.temp_f);
 }
 
 getCity();
